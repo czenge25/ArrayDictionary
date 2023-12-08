9976ef30712097c6be643a79bb5b4d57b69cf4e4
czenge25 <czenge25@kentdenver.org>
2023-12-08 12:09:38 -0700

    Created driver and added comments

--- ArrayDictionary/src/ArrayDictionary.java
+++ ArrayDictionary/src/ArrayDictionary.java
@@ -3,64 +3,65 @@ import java.util.Collection;
 
 public class ArrayDictionary<E,T> {
 
-    private ArrayList<E> keys;
-    private ArrayList<T> values;
+    // ArrayLists to store keys and values
+    private ArrayList<E> keys = new ArrayList<E>();
+    private ArrayList<T> values = new ArrayList<T>();
+
+    // Size variable to keep track of the number of key-value pairs in the dictionary
     private int size;
 
+    // Default constructor, dictionary starts with size = 0
     public ArrayDictionary() {
 
     }
 
-    //add an key-value pair to the dictionary
+    // Method to add a key-value pair to the dictionary
     public void put(E key, T value) {
         keys.add(key);
         values.add(value);
         size++;
     }
 
-    //get the value associated with a given key
+    // Method to get the value associated with a given key
     public T get(E key) {
         return values.get(keys.indexOf(key));
     }
 
-    //remove a key-value pair and return its value
+    // Method to remove a key-value pair and return its value
     public T remove(E key) {
-        keys.remove(key);
-        return values.remove(keys.indexOf(key));
-        size--;
+        int index = keys.indexOf(key);
+        if (index != -1) {
+            keys.remove(index);
+            size--;
+            return values.remove(index);
+        }
+        // If key is not found, prints a warning message and returns null
+        System.out.println("Could not remove key \"" + key + "\": Dictionary will have less than 0 keys");
+        return null;
     }
 
-    //returns true if the given key has an associated value
+    // Method to check if the given key has an associated value
     public boolean contains(E key) {
-        if (values.contains(keys.indexOf(key))) {
-            return true;
-        } else {
-            return false;
-        }
+        return keys.contains(key);
     }
 
-    //returns true if the dictionary is empty
+    // Method to check if the dictionary is empty
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == 0;
     }
 
-    //returns the number of key-value pairs in the dictionary
+    // Method to get the number of key-value pairs in the dictionary
     public int size() {
-        return size();
+        return size;
     }
 
-    //returns a Collection of keys
+    // Method to get a Collection of keys
     public Collection<E> keys() {
         return keys;
     }
 
-    //returns a Collection of values
+    // Method to get a Collection of values
     public Collection<T> values() {
         return values;
     }
-
 }
